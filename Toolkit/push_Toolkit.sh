4bfa6d039a589c794eb7398e227584624dcf2bf2
DIscover304 <hobart.yang@qq.com>
2020-04-28 03:18:04 +0800

    Part 1: This is a small change by master.

--- Toolkit/push_Toolkit.sh
+++ Toolkit/push_Toolkit.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+stty -echo
+echo -e "\033[7m这是git提交机，按任意键继续\033[0m"
+echo -e "\033[7mThis is a git machine, press any key to continue\033[0m"
+read -n1
 
+echo "-------------------status-----------------------"
 git status
+echo "-------------------pulling----------------------"
 git pull
-echo "你有大概8秒时间确认/You have about 8s to have a check"
-for i in {1..2};
-do
-    ping 192.0.2.2 -n 1 > nul;
-done
 
-git add Toolkit
+echo
+echo -e "\033[7m请确认，按任意键继续\033[0m"
+echo -e "\033[7mPlease, have a check, press any key to continue\033[0m"
+read -n1
+
+echo "-------------------adding-----------------------"
+git add .
+echo "-------------------commiting--------------------"
 git commit -m "Part 1: This is a small change by master."
+echo "-------------------pushing----------------------"
 git push origin master
+echo "-------------------status again-----------------"
 git status
 
-echo "1分钟，再检查一下/1m, check again"
-ping 192.0.2.2 -n 1 -w 60000 > nul
+echo
+echo -e "\033[7m检查一下, 按任意键退出\033[0m"
+echo -e "\033[7m/check again, press any key to exit\033[0m"
+read -n1
