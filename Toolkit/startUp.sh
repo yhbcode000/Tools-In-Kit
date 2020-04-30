bb0803a2d4be84f0277568210c84be22123d9cfb
Hobart <hobart.yang@qq.com>
2020-04-30 17:09:55 +0800

    Part 1: This is a small change by master.

--- Toolkit/startUp.sh
+++ Toolkit/startUp.sh
@@ -24,13 +24,13 @@ work (){
     #echo -e "Starting weChat for Biz(企业微信)"
     #start "" "C:\Program Files (x86)\WXWork\WXWork.exe"
     
-    sleep 0.1
-    echo -e "Starting Zoom(zoom 会议室)"
-    start "" "C:\Users\22759\AppData\Roaming\Zoom\bin\Zoom.exe"
-    
     #sleep 0.1
-    #echo -e "Starting Bilibili Live(哔哩哔哩直播间)"
-    #start "" "C:\Program Files (x86)\bililive\livehime\livehime.exe"
+    #echo -e "Starting Zoom(zoom 会议室)"
+    #start "" "C:\Users\22759\AppData\Roaming\Zoom\bin\Zoom.exe"
+    
+    sleep 0.1
+    echo -e "Starting Bilibili Live(哔哩哔哩直播间)"
+    start "" "C:\Program Files (x86)\bililive\livehime\livehime.exe"
     
     sleep 0.1
     echo -e "Starting Microsoft Teams(微软 Teams)"
